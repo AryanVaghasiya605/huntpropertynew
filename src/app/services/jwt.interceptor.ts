fa32aaf02c0bd5d58b3c75b83e9ca4fbb944542e
naveen katiyar <naveenkatiyar@gmail.com>
2025-01-26 13:10:09 +0530

    naveen push

--- src/app/services/jwt.interceptor.ts
+++ src/app/services/jwt.interceptor.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
 
 @Injectable()
-export class JwtInterceptor implements HttpInterceptor {
-
-  constructor() {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private _nativeStorage:NativeStorage){}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Add Authorization header to the request
+    this._nativeStorage.getItem('token').then((token)=>{
+     // console.log('token', token);
+      
+    })
+    
+    const getToken = 'token';
+    const authRequest = request.clone({
+      setHeaders: { Authorization: `Bearer ${getToken}` }
+    });
+  //  console.log(authRequest)
+    return next.handle(authRequest);
   }
 }
